87797c0646758a51d707f1031306369d0f124de5
João Correia <j.correia2213@gmail.com>
2022-03-09 15:39:52 +0000

    Adição da Documentação e Projeto Inicial
    - Criação do repositório EAD
    - Criação do projeto inicial
    - Criação da Documentação do projeto

---
+++ EAD_TP/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <conio.h>
+
+main() {
+	char* locale;
+	locale = setlocale(LC_ALL, "");
+	/*!
+	* Teste 123
+	*/
+	printf("Teste João");
+}
+
+void menu() { //asdiansdjansd
+
+}
